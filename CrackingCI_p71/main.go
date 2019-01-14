4cec0c65bcb2f79e2c6cb6cfe67a68f360eb9299
David <szabvid@gmail.com>
2019-01-14 22:23:44 +0100

    Adding string permutation in imperative way.

--- CrackingCI_p71/main.go
+++ CrackingCI_p71/main.go
@@ -6,6 +6,30 @@ package main
 import "fmt"
 
 func perm(w []string) []string {
+	res := []string{""}
+	tmp := []string{}
+
+	for i := 0; i < len(w); i++ { // each letter
+		tmp = []string{}
+		for j := 0; j < len(res); j++ { // each permutation variant
+			for k := 0; k <= len(res[j]); k++ { // each insertion place
+				tmp = append(tmp, res[j][0:k]+w[i]+res[j][k:])
+			}
+		}
+		res = tmp
+	}
+
+	return res
+}
+
+func main() {
+	for _, elem := range perm([]string{"a", "b", "c"}) {
+		fmt.Println(elem)
+	}
+}
+
+/*
+func perm(w []string) []string {
 	if len(w) == 0 {
 		return []string{""}
 	}
@@ -28,3 +52,4 @@ func main() {
 	perm(w[:])
 	fmt.Printf("Hello %s", "alma")
 }
+*/
